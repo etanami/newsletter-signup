00a518ffd764f584700ccb627b0db797b2644a1a
Etanami Olatunji <etanamiolatunji@gmail.com>
2022-07-10 22:44:24 -0700

    refactor: posted user data to mailchimp servers

--- app.js
+++ app.js
@@ -1,15 +1,65 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-const bootstrap = require('bootstrap');
+const https = require('https');
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/signup.html');
+})
 
+app.post('/', (req, res) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]     
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = 'https://us17.api.mailchimp.com/3.0/lists/d978f7f85e';
+    const options = {
+        mehod: 'POST',
+        auth: 'etanamie:7e495aad59fdbc9ee0ccd09d1e8341ee-us17'
+    }
+
+    const request = https.request(url, options, (response) => {
+
+        response.on('data', (data) => {
+            console.log(JSON.parse(data));
+        })
+    })
+
+
+    request.write(jsonData);
+    request.end();
+
+})
 
 
 app.listen(3000, ()=> {
     console.log('Server listening on port 3000!');
 })
+
+
+// MailChimp Api key
+// 7e495aad59fdbc9ee0ccd09d1e8341ee-us17
+
+// list ID
+// d978f7f85e
